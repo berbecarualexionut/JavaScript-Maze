b1d33439d7f5ddd7a3a97b089332838a685e55d8
berbecaru.alex <berbecaru.alex.ionut@gmail.com>
2019-03-11 22:39:58 +0200

    Added sounds

--- maze.js
+++ maze.js
@@ -4,21 +4,32 @@ var start = document.getElementById("start");
 var end = document.querySelector("#end");
 var status = document.querySelector("#status");
 var win = true;
+var audio = new Audio('Bike Horn-SoundBible.com-602544869.mp3');
+var sounds = '';
 
-  start.addEventListener("mouseover", function() {
+
+  start.addEventListener("mouseover", function(event) {
+    sounds = playSounds();
     document.getElementById("status").innerHTML = "Move your mouse over the S to begin";
     for (var i = 0; i < boundaries.length; i++) {
       boundaries[i].addEventListener("mouseover", function() {
         win = false;
         this.style.background = "red";
+        clearInterval(sounds);
         alert("YOU LOSE! START OVER!");
         this.style.background = "#eeeeee";
         document.getElementById("status").innerHTML = "YOU LOSE!";
-        stopPropagation();
+        event.stopPropagation();
       });
     }
 
   });
+
+function playSounds(){
+  setInterval(function(){
+    audio.play();
+  },4000);
+}
 
 end.addEventListener("mouseover", function() {
   if (win == true) {
